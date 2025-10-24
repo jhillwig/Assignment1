f22984cf5a78ead05cb341459c7b53ab4cd24b12
jhillwig <jhillwig@kent.edu>
2025-10-24 16:15:15 -0400

    Completed producer.c
    Completed all parts needed in producer.c

--- producer.c
+++ producer.c
@@ -31,12 +31,43 @@
       srand(time(NULL));
 
     // Setting up Shared Memory
+      int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+      ftruncate(shm_fd, sizeof(Table));
+      table = mmap(NULL, sizeof(Table), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+      table->in = 0;
+      table->out = 0;
       
     // Intialization of Semaphores
+      sem_t *empty = sem_open(SEM_EMPTY, O_CREAT, 0666, TABLE_SIZE);
+      sem_t *full = sem_open(SEM_FULL, O_CREAT, 0666, 0);
+      sem_t *mutex = sem_open(SEM_MUTEX, O_CREATE, 0666, 1);
 
     // Printing Process Initiation
+      printf("[Producer]: Starting Production...\n");
 
     // While loop for iterations
+      int loop_it = 0;
+      while(s_running && (loop_it < ITERATIONS)) {
+        // Item Generation
+          int item = addItem();
+
+        // Wait until empty slot is availible & for mutual exclusion (Decrement for semaphores)
+          sem_wait(empty);
+          sem_wait(mutex);
+
+        // Putting item onto table
+          table->buffer[table->in] = item;
+          printf("[Producer]: Produced %d at position %d\n", item, table->in);
+          table->in = (table->in + 1) % TABLE_SIZE;
+
+        // Releasing mutex lock for a new process and signals a new spot is open (Increment for semaphores)
+          sem_post(mutex);
+          sem_post(full);
+        
+        // Increasing iterations and 
+          loop_it++;
+          sleep((rand() % 2) + 1);
+      }
 
     // Run cleanup() at the end of main then return 0
       cleanup();
@@ -73,7 +104,6 @@
       sem_unlink(SEM_MUTEX);
       sem_Unlink(SHM_NAME);
 
-      printf("[Producer]: Exiting cleanly...\n");
+    // Printing Exit message
+      printf("[Producer]: Exiting...\n");
   }
-
-
